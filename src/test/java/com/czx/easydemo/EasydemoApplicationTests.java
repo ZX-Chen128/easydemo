bd87ed72ea7e23ac56b16050f36c2d5b64163ac6
’rylynnczxshuaidai’ <477895675@qq.com>
2020-04-23 11:06:00 +0800

    easy demo

---
+++ src/test/java/com/czx/easydemo/EasydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czx.easydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
